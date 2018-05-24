108a010b8f640aa981a64bedf87cbfbf8e7d46f6
Amaresh Menchu <Amaresh.Menchu@copart.com>
2018-05-24 17:21:12 +0530

    modified pkg names

--- spring-boot-fcm-framework/src/main/java/com/copart/framework/fcm/FcmApplication.java
+++ spring-boot-fcm-framework/src/main/java/com/copart/framework/fcm/FcmApplication.java
@@ -1,14 +1,7 @@
 package com.copart.framework.fcm;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.copart.framework.fcm.callback.FcmStanzaListener;
-import com.copart.framework.fcm.client.FcmCcsClient;
-import com.copart.framework.fcm.properties.ApplicationProperties;
 
 @SpringBootApplication
 public class FcmApplication {
--- spring-boot-fcm-framework/src/test/java/com/copart/framework/fcm/FlexibleBackendApplicationTests.java
+++ spring-boot-fcm-framework/src/test/java/com/copart/framework/fcm/FlexibleBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.copart.fcm.framework;
+package com.copart.framework.fcm;
 
 import org.junit.Ignore;
 import org.junit.Test;
