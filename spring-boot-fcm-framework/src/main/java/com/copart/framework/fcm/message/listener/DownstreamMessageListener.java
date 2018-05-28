f31ab9ff3db19d1f97847a7b80f87320447e6d52
Amaresh Menchu <Amaresh.Menchu@copart.com>
2018-05-28 12:40:25 +0530

    renamed varible names

--- spring-boot-fcm-framework/src/main/java/com/copart/framework/fcm/message/listener/DownstreamMessageListener.java
+++ spring-boot-fcm-framework/src/main/java/com/copart/framework/fcm/message/listener/DownstreamMessageListener.java
@@ -23,21 +23,21 @@ public class DownstreamMessageListener extends DeferringFcmMessageListener<Downs
 			.getLogger(DownstreamMessageListener.class.getSimpleName());
 
 	@Override
-	public void onReceiveMessage(DownstreamMessage downstreamMessageResponse) {
-		super.onReceiveMessage(downstreamMessageResponse);
-		if (downstreamMessageResponse.getMessageType().equals("ack")) {
-			handleAckReceipt(downstreamMessageResponse);
+	public void onReceiveMessage(DownstreamMessage downstreamMessage) {
+		super.onReceiveMessage(downstreamMessage);
+		if (downstreamMessage.getMessageType().equals("ack")) {
+			handleAckReceipt(downstreamMessage);
 		} else {
-			handleNackReceipt(downstreamMessageResponse);
+			handleNackReceipt(downstreamMessage);
 		}
 	}
 
 	/**
 	 * Handles a NACK message from FCM
 	 */
-	public void handleNackReceipt(DownstreamMessage downstreamMessageResponse) {
+	public void handleNackReceipt(DownstreamMessage downstreamMessage) {
 		logger.info("<<<< Nack receipt received for a Downstream Message sent. <<<<");
-		String errorCode = downstreamMessageResponse.getError();
+		String errorCode = downstreamMessage.getError();
 
 		if (errorCode == null) {
 			logger.info("Received null FCM Error Code");
@@ -46,28 +46,28 @@ public class DownstreamMessageListener extends DeferringFcmMessageListener<Downs
 
 		switch (errorCode) {
 		case INVALID_JSON:
-			handleUnrecoverableFailure(downstreamMessageResponse);
+			handleUnrecoverableFailure(downstreamMessage);
 			break;
 		case BAD_REGISTRATION:
-			handleUnrecoverableFailure(downstreamMessageResponse);
+			handleUnrecoverableFailure(downstreamMessage);
 			break;
 		case DEVICE_UNREGISTERED:
-			handleUnrecoverableFailure(downstreamMessageResponse);
+			handleUnrecoverableFailure(downstreamMessage);
 			break;
 		case BAD_ACK:
-			handleUnrecoverableFailure(downstreamMessageResponse);
+			handleUnrecoverableFailure(downstreamMessage);
 			break;
 		case SERVICE_UNAVAILABLE:
-			handleServerFailure(downstreamMessageResponse);
+			handleServerFailure(downstreamMessage);
 			break;
 		case INTERNAL_SERVER_ERROR:
-			handleServerFailure(downstreamMessageResponse);
+			handleServerFailure(downstreamMessage);
 			break;
 		case DEVICE_MESSAGE_RATE_EXCEEDED:
-			handleUnrecoverableFailure(downstreamMessageResponse);
+			handleUnrecoverableFailure(downstreamMessage);
 			break;
 		case TOPICS_MESSAGE_RATE_EXCEEDED:
-			handleUnrecoverableFailure(downstreamMessageResponse);
+			handleUnrecoverableFailure(downstreamMessage);
 			break;
 		case CONNECTION_DRAINING:
 			handleConnectionDrainingFailure();
@@ -80,21 +80,21 @@ public class DownstreamMessageListener extends DeferringFcmMessageListener<Downs
 	/**
 	 * Handles an ACK message from FCM
 	 */
-	public void handleAckReceipt(DownstreamMessage downstreamMessageResponse) {
+	public void handleAckReceipt(DownstreamMessage downstreamMessage) {
 		logger.info("<<<< Ack receipt received for a Downstream Message sent. <<<<");
 	}
 
-	public void handleServerFailure(DownstreamMessage downstreamMessageResponse) {
+	public void handleServerFailure(DownstreamMessage downstreamMessage) {
 		// TODO: Resend the message
-		logger.info("Server error: " + downstreamMessageResponse.getError() + " -> "
-				+ downstreamMessageResponse.getErrorDescription());
+		logger.info("Server error: " + downstreamMessage.getError() + " -> "
+				+ downstreamMessage.getErrorDescription());
 
 	}
 
-	public void handleUnrecoverableFailure(DownstreamMessage downstreamMessageResponse) {
+	public void handleUnrecoverableFailure(DownstreamMessage downstreamMessage) {
 		// TODO: handle the unrecoverable failure
-		logger.info("Unrecoverable error: " + downstreamMessageResponse.getError() + " -> "
-				+ downstreamMessageResponse.getErrorDescription());
+		logger.info("Unrecoverable error: " + downstreamMessage.getError() + " -> "
+				+ downstreamMessage.getErrorDescription());
 	}
 
 	public void handleConnectionDrainingFailure() {
